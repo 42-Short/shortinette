6886253d31855ca99068cf5f4cb1a71b76744dc3
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-06-25 05:36:02 +0000

    add: compile with dummy lib to check for forbidden functions

--- pkg/functioncheck/functioncheck.go
+++ pkg/functioncheck/functioncheck.go
@@ -2,10 +2,12 @@ package functioncheck
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 )
 
-func Execute(allowedFunctions []string) error {
+func writeDummyLib(allowedFunctions []string) error {
 	filePath := "allowed_functions/src/lib.rs"
 	file, err := os.Create(filePath)
 	if err != nil {
@@ -34,5 +36,32 @@ func Execute(allowedFunctions []string) error {
 			}
 		}
 	}
+	return nil
+}
+
+func compileWithDummyLib(studentProjectPath string) error {
+	cmd := exec.Command("sh", "-c", fmt.Sprintf("echo '[dependencies]\nallowed_functions = { path = \"../allowed_functions\" }\n' >> %s/Cargo.toml", studentProjectPath))
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	cmd = exec.Command("cargo", "build", "--manifest-path", fmt.Sprintf("%s/Cargo.toml", studentProjectPath))
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	log.Println("Compilation successful")
+	return nil
+}
+
+func Execute(allowedFunctions []string) error {
+	if err := writeDummyLib(allowedFunctions); err != nil {
+		return err
+	}
+
+	if err := compileWithDummyLib("ex00"); err != nil {
+		return err
+	}
+
 	return nil
 }
