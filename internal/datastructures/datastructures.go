0abe793a1027b8dd867b87fc9b568097f8cb22a7
winstonallo <arthurbiedchar@gmail.com>
2024-06-26 19:05:52 +0200

    add: data structures for tests

--- internal/datastructures/datastructures.go
+++ internal/datastructures/datastructures.go
@@ -1,5 +1,23 @@
 package datastructures
 
+type Test struct {
+	Output struct {
+		AssertEq []string `yaml:"assert_eq"`
+		AssertNe []string `yaml:"assert_ne"`
+	}
+}
+
+type Exercise struct {
+	TurnInDirectory string   `yaml:"turn_in_directory"`
+	TurnInFiles     []string `yaml:"turn_in_files"`
+	AllowedItems    []string `yaml:"allowed_items"`
+	Tests           Test     `yaml:"tests"`
+}
+
+type Config struct {
+	Ex00 Exercise `yaml:"ex00"`
+}
+
 type AllowedItem struct {
 	Name string
 	Type string
