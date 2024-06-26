4078fe5ab7522f98c478dd3393a77eadddf11958
winstonallo <arthurbiedchar@gmail.com>
2024-06-26 21:24:59 +0200

    add: defer to ensure compile-environment is always cleaned up

--- pkg/functioncheck/functioncheck.go
+++ pkg/functioncheck/functioncheck.go
@@ -119,9 +119,17 @@ func handleCompileError(output string) error {
 }
 
 func Execute(allowedItems []datastructures.AllowedItem, exercise string) (err error) {
+	defer func() {
+		rmErr := os.RemoveAll("compile-environment/")
+		if rmErr != nil {
+			err = fmt.Errorf("failed to remove compile environment: %w", rmErr)
+		}
+	}()
+
 	if err = initCompilingEnvironment(allowedItems, exercise); err != nil {
 		return err
 	}
+	
 
 	if err = git.Get("https://github.com/42-Short/shortinette-test.git", "compile-environment/src/"); err != nil {
 		return err
@@ -137,9 +145,6 @@ func Execute(allowedItems []datastructures.AllowedItem, exercise string) (err er
 		return handleCompileError(output)
 	}
 
-	if err = os.RemoveAll("compile-environment/"); err != nil {
-		return fmt.Errorf("failed to remove compile environment: %w", err)
-	}
 
 	return nil
 }
