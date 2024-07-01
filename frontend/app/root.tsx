4c301a44030287f9bc98d97ad62c0af5e7b8527f
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2024-07-01 11:46:38 +0200

    add: main page with carousel for each day of the piscine

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -31,6 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <NavBar />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -7,6 +7,7 @@ import {
   CarouselPrevious,
 } from "~/components/ui/carousel";
 import { Card, CardContent } from "~/components/ui/card";
+import { H1 } from "~/components/ui/H1";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   return null;
@@ -14,24 +15,32 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export default function Index() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Carousel className="w-full max-w-xl md:max-w-3xl h-96">
-        <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index}>
-              <div className="p-1">
-                <Card>
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <H1>Rust Piscine</H1>
+      <div className="w-full max-w-xl md:max-w-3xl h-96">
+        <Carousel>
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card>
+                    <div className="px-4 py-2 bg-gray-200">
+                      <h3 className="text-lg font-semibold">Day {index + 1}</h3>
+                    </div>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-4xl font-semibold">
+                        {index + 1}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
     </div>
   );
 }
