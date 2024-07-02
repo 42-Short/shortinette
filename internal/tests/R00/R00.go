a8584cf301943c55c9e016fcc85b7b47ab37ba08
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2024-07-02 23:24:30 +0200

    fix: unchecked return value in imodule.SetUp

--- internal/tests/R00/R00.go
+++ internal/tests/R00/R00.go
@@ -1,14 +1,17 @@
 package R00
 
 import (
-	"github.com/42-Short/shortinette/internal/interfaces/module"
 	"github.com/42-Short/shortinette/internal/interfaces/exercise"
+	"github.com/42-Short/shortinette/internal/interfaces/module"
+	"github.com/42-Short/shortinette/internal/logger"
 )
 
 func R00(repoId string, codeDirectory string) {
 	r00 := modulebuilder.NewModuleBuilder().
 		SetName("R00").
 		SetExercises([]exercisebuilder.ExerciseBuilder{ex00(), ex01()})
-	r00.SetUp(repoId, codeDirectory)
+	if err := r00.SetUp(repoId, codeDirectory); err != nil {
+		logger.Error.Printf("internal error: %v", err)
+	}
 	r00.Run()
 }
