4ffa9558c4d3cd8f932c4a5b68c11213d3a16ae3
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2024-07-05 10:58:08 +0200

    add: startup scriupt

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"github.com/42-Short/shortinette/internal/logger"
 	"github.com/42-Short/shortinette/internal/tests/R00"
+	"github.com/42-Short/shortinette/internal/tests/testutils"
 	"github.com/42-Short/shortinette/internal/utils"
 )
 
 func main() {
 	logger.InitializeStandardLoggers()
 	if err := utils.RequireEnv(); err != nil {
+		logger.Error.Println(err.Error())
+		return
+	}
+	if _, err := testutils.RunCode("./scripts/check_dependencies.sh"); err != nil {
 		logger.Error.Println(err.Error())
 		return
 	}
