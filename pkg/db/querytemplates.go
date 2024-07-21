de610563d0d90291cb3a23991695b8c0ad0f87ba
winstonallo <arthurbiedchar@gmail.com>
2024-07-21 16:11:20 +0200

    fix: swapped repo.Score and repo.Attempt in readme result template, attempts now defualt at 1

--- pkg/db/querytemplates.go
+++ pkg/db/querytemplates.go
@@ -11,7 +11,7 @@ type Templates struct {
 var sqliteTemplates = Templates{
 	RepoById:        "SELECT id, first_attempt, last_graded, wait_time, score, attempts FROM %s WHERE id = ?",
 	TableByName:     "SELECT name FROM sqlite_master WHERE type='table' AND name='%s'",
-	CreateRepoTable: "CREATE TABLE %s (id TEXT PRIMARY KEY, first_attempt BOOLEAN DEFAULT 1, last_graded TEXT DEFAULT (datetime('now')), wait_time INTEGER DEFAULT 0, score INTEGER DEFAULT 0, attempts INTEGER DEFAULT 0)",
+	CreateRepoTable: "CREATE TABLE %s (id TEXT PRIMARY KEY, first_attempt BOOLEAN DEFAULT 1, last_graded TEXT DEFAULT (datetime('now')), wait_time INTEGER DEFAULT 0, score INTEGER DEFAULT 0, attempts INTEGER DEFAULT 1)",
 	UpdateRepo:      "UPDATE %s SET first_attempt = ?, last_graded = ?, wait_time = ?, score = ?, attempts = ? WHERE id = ?",
 	NewRepoRow:      "INSERT INTO %s (id) VALUES(?)",
 }
