5bd5625d52f047d9d8584983ad70aa8d42105379
winstonallo <arthurbiedchar@gmail.com>
2024-08-05 15:59:27 +0200

    fix: comment out unused code

--- archived/functioncheck/alloweditems.go
+++ archived/functioncheck/alloweditems.go
@@ -1,66 +1,66 @@
 package functioncheck
 
-import (
-	"fmt"
-	"os"
-	"path/filepath"
+// import (
+// 	"fmt"
+// 	"os"
+// 	"path/filepath"
 
-	"github.com/42-Short/shortinette/internal/templates"
-	Exercise "github.com/42-Short/shortinette/pkg/interfaces/exercise"
-)
+// 	"github.com/42-Short/shortinette/internal/templates"
+// 	Exercise "github.com/42-Short/shortinette/pkg/interfaces/exercise"
+// )
 
-func writeTemplateToFile(template, itemName string, file *os.File) error {
-	content := fmt.Sprintf(template, itemName, itemName)
-	if _, err := file.WriteString(content); err != nil {
-		return fmt.Errorf("error writing entry: %w", err)
-	}
-	return nil
-}
+// func writeTemplateToFile(template, itemName string, file *os.File) error {
+// 	content := fmt.Sprintf(template, itemName, itemName)
+// 	if _, err := file.WriteString(content); err != nil {
+// 		return fmt.Errorf("error writing entry: %w", err)
+// 	}
+// 	return nil
+// }
 
-func writeAllowedItemsLib(test Exercise.Exercise, file *os.File, exercise string) error {
-	content := fmt.Sprintf(templates.AllowedItemsLibHeader, exercise)
-	if _, err := file.WriteString(content); err != nil {
-		return err
-	}
+// func writeAllowedItemsLib(test Exercise.Exercise, file *os.File, exercise string) error {
+// 	content := fmt.Sprintf(templates.AllowedItemsLibHeader, exercise)
+// 	if _, err := file.WriteString(content); err != nil {
+// 		return err
+// 	}
 
-	for _, macro := range test.AllowedMacros {
-		if err := writeTemplateToFile(templates.AllowedMacroTemplate, macro, file); err != nil {
-			return err
-		}
-	}
-	for _, function := range test.AllowedFunctions {
-		if err := writeTemplateToFile(templates.AllowedFunctionTemplate, function, file); err != nil {
-			return err
-		}
-	}
+// 	for _, macro := range test.AllowedMacros {
+// 		if err := writeTemplateToFile(templates.AllowedMacroTemplate, macro, file); err != nil {
+// 			return err
+// 		}
+// 	}
+// 	for _, function := range test.AllowedFunctions {
+// 		if err := writeTemplateToFile(templates.AllowedFunctionTemplate, function, file); err != nil {
+// 			return err
+// 		}
+// 	}
 
-	if _, err := file.WriteString("}"); err != nil {
-		return fmt.Errorf("error writing closing bracket: %w", err)
-	}
-	return nil
-}
+// 	if _, err := file.WriteString("}"); err != nil {
+// 		return fmt.Errorf("error writing closing bracket: %w", err)
+// 	}
+// 	return nil
+// }
 
-func createFileWithDirs(filePath string) (*os.File, error) {
-	dir := filepath.Dir(filePath)
-	if err := os.MkdirAll(dir, 0755); err != nil {
-		return nil, err
-	}
+// func createFileWithDirs(filePath string) (*os.File, error) {
+// 	dir := filepath.Dir(filePath)
+// 	if err := os.MkdirAll(dir, 0755); err != nil {
+// 		return nil, err
+// 	}
 
-	file, err := os.Create(filePath)
-	if err != nil {
-		return nil, err
-	}
-	return file, nil
-}
+// 	file, err := os.Create(filePath)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return file, nil
+// }
 
-func writeCargoToml(filePath, content string) error {
-	file, err := createFileWithDirs(filePath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	if _, err := file.WriteString(content); err != nil {
-		return err
-	}
-	return nil
-}
+// func writeCargoToml(filePath, content string) error {
+// 	file, err := createFileWithDirs(filePath)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer file.Close()
+// 	if _, err := file.WriteString(content); err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
--- archived/functioncheck/functioncheck.go
+++ archived/functioncheck/functioncheck.go
@@ -1,133 +1,133 @@
 package functioncheck
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/42-Short/shortinette/internal/templates"
-	Exercise "github.com/42-Short/shortinette/pkg/interfaces/exercise"
-)
+// 	"github.com/42-Short/shortinette/internal/templates"
+// 	Exercise "github.com/42-Short/shortinette/pkg/interfaces/exercise"
+// )
 
-func initCompilingEnvironment(test Exercise.Exercise, exercise string) error {
-	libFilePath := "compile-environment/allowedfunctions/src/lib.rs"
-	file, err := createFileWithDirs(libFilePath)
-	if err != nil {
-		return err
-	}
-
-	if err := writeAllowedItemsLib(test, file, exercise); err != nil {
-		return err
-	}
-
-	if err := writeCargoToml("compile-environment/allowedfunctions/Cargo.toml", templates.AllowedItemsCargoToml); err != nil {
-		return err
-	}
-
-	cargoTomlContent := fmt.Sprintf(templates.CargoTomlTemplate, "compile-environment", exercise)
-	if err := writeCargoToml("compile-environment/Cargo.toml", cargoTomlContent); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// func prependHeadersToStudentCode(filePath, exerciseNumber string, exerciseType string, dummyCall string) error {
-// 	originalFile, err := os.Open(filePath)
-// 	if err != nil {
-// 		return fmt.Errorf("could not open original file: %w", err)
-// 	}
-// 	defer originalFile.Close()
-
-// 	tempFilePath := fmt.Sprintf("compile-environment/src/%s/temp.rs", exerciseNumber)
-// 	tempFile, err := os.Create(tempFilePath)
-// 	if err != nil {
-// 		return fmt.Errorf("could not create temp file: %w", err)
-// 	}
-// 	defer tempFile.Close()
-
-// 	headers := fmt.Sprintf(templates.StudentCodePrefix, exerciseNumber)
-
-// 	if _, err := tempFile.WriteString(headers); err != nil {
-// 		return fmt.Errorf("could not write headers: %w", err)
-// 	}
-// 	originalContent, err := io.ReadAll(originalFile)
-// 	if err != nil {
-// 		return fmt.Errorf("could not read original file content: %w", err)
-// 	}
-// 	if _, err := tempFile.Write(originalContent); err != nil {
-// 		return fmt.Errorf("could not write original content to temp file: %w", err)
-// 	}
-
-// 	if exerciseType == "function" {
-// 		main := fmt.Sprintf(templates.DummyMain, dummyCall)
-// 		if _, err := tempFile.Write([]byte(main)); err != nil {
-// 			return fmt.Errorf("could not write dummy main to temp file: %w", err)
-// 		}
-// 	}
-// 	return nil
-// }
-
-// func compileWithDummyLib(sourceDir string) (string, error) {
-// 	cmd := exec.Command("cargo", "build")
-// 	cmd.Dir = sourceDir
-
-// 	output, err := cmd.CombinedOutput()
-// 	if err != nil {
-// 		return string(output), fmt.Errorf("error compiling code in %s: %w\nCompiler Output:\n%s", sourceDir, err, output)
-// 	}
-// 	return string(output), nil
-// }
-
-// func setToSlice(forbiddenFunctionSet map[string]bool) []string {
-// 	var slice []string
-// 	for key := range forbiddenFunctionSet {
-// 		slice = append(slice, key)
-// 	}
-// 	return slice
-// }
-
-// func parseForbiddenFunctions(compilerOutput string) ([]string, error) {
-// 	re, err := regexp.Compile(`error: cannot find (function|macro) ` + "`(\\w+)`" + ` in this scope`)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("error compiling regex: %w", err)
-// 	}
-// 	matches := re.FindAllStringSubmatch(compilerOutput, -1)
-// 	if matches == nil {
-// 		return nil, nil
-// 	}
-
-// 	forbiddenFunctionsSet := make(map[string]bool)
-
-// 	for _, match := range matches {
-// 		if len(match) > 2 {
-// 			forbiddenFunctionsSet[match[2]] = true
-// 		}
-// 	}
-
-// 	return setToSlice(forbiddenFunctionsSet), nil
-// }
-
-// func handleCompileError(output string) error {
-// 	usedForbiddenFunctions, parseErr := parseForbiddenFunctions(output)
-// 	if parseErr != nil {
-// 		return parseErr
-// 	} else if len(usedForbiddenFunctions) > 0 {
-// 		forbiddenFunctions := strings.Join(usedForbiddenFunctions, ", ")
-// 		return errors.NewSubmissionError(errors.ErrForbiddenItem, forbiddenFunctions)
-// 	} else {
-// 		return errors.NewSubmissionError(errors.ErrInvalidCompilation, output)
-// 	}
-// }
-
-// TODO
-// Currently only handles linting for keywords - code for conditional compilation stands,
-// just needs to be adapted a little bit.
-// func Execute(test Exercise.Exercise) (err error) {
-// 	exercisePath := fmt.Sprintf("compile-environment/%s/%s", test.TurnInDirectory, test.TurnInFiles[0])
-// 	err = lintStudentCode(exercisePath, test)
+// func initCompilingEnvironment(test Exercise.Exercise, exercise string) error {
+// 	libFilePath := "compile-environment/allowedfunctions/src/lib.rs"
+// 	file, err := createFileWithDirs(libFilePath)
 // 	if err != nil {
 // 		return err
 // 	}
 
-// 	logger.Info.Printf("no forbidden items/keywords found in %s", test.TurnInDirectory+"/"+test.TurnInFiles[0])
+// 	if err := writeAllowedItemsLib(test, file, exercise); err != nil {
+// 		return err
+// 	}
+
+// 	if err := writeCargoToml("compile-environment/allowedfunctions/Cargo.toml", templates.AllowedItemsCargoToml); err != nil {
+// 		return err
+// 	}
+
+// 	cargoTomlContent := fmt.Sprintf(templates.CargoTomlTemplate, "compile-environment", exercise)
+// 	if err := writeCargoToml("compile-environment/Cargo.toml", cargoTomlContent); err != nil {
+// 		return err
+// 	}
+
 // 	return nil
 // }
+
+// // func prependHeadersToStudentCode(filePath, exerciseNumber string, exerciseType string, dummyCall string) error {
+// // 	originalFile, err := os.Open(filePath)
+// // 	if err != nil {
+// // 		return fmt.Errorf("could not open original file: %w", err)
+// // 	}
+// // 	defer originalFile.Close()
+
+// // 	tempFilePath := fmt.Sprintf("compile-environment/src/%s/temp.rs", exerciseNumber)
+// // 	tempFile, err := os.Create(tempFilePath)
+// // 	if err != nil {
+// // 		return fmt.Errorf("could not create temp file: %w", err)
+// // 	}
+// // 	defer tempFile.Close()
+
+// // 	headers := fmt.Sprintf(templates.StudentCodePrefix, exerciseNumber)
+
+// // 	if _, err := tempFile.WriteString(headers); err != nil {
+// // 		return fmt.Errorf("could not write headers: %w", err)
+// // 	}
+// // 	originalContent, err := io.ReadAll(originalFile)
+// // 	if err != nil {
+// // 		return fmt.Errorf("could not read original file content: %w", err)
+// // 	}
+// // 	if _, err := tempFile.Write(originalContent); err != nil {
+// // 		return fmt.Errorf("could not write original content to temp file: %w", err)
+// // 	}
+
+// // 	if exerciseType == "function" {
+// // 		main := fmt.Sprintf(templates.DummyMain, dummyCall)
+// // 		if _, err := tempFile.Write([]byte(main)); err != nil {
+// // 			return fmt.Errorf("could not write dummy main to temp file: %w", err)
+// // 		}
+// // 	}
+// // 	return nil
+// // }
+
+// // func compileWithDummyLib(sourceDir string) (string, error) {
+// // 	cmd := exec.Command("cargo", "build")
+// // 	cmd.Dir = sourceDir
+
+// // 	output, err := cmd.CombinedOutput()
+// // 	if err != nil {
+// // 		return string(output), fmt.Errorf("error compiling code in %s: %w\nCompiler Output:\n%s", sourceDir, err, output)
+// // 	}
+// // 	return string(output), nil
+// // }
+
+// // func setToSlice(forbiddenFunctionSet map[string]bool) []string {
+// // 	var slice []string
+// // 	for key := range forbiddenFunctionSet {
+// // 		slice = append(slice, key)
+// // 	}
+// // 	return slice
+// // }
+
+// // func parseForbiddenFunctions(compilerOutput string) ([]string, error) {
+// // 	re, err := regexp.Compile(`error: cannot find (function|macro) ` + "`(\\w+)`" + ` in this scope`)
+// // 	if err != nil {
+// // 		return nil, fmt.Errorf("error compiling regex: %w", err)
+// // 	}
+// // 	matches := re.FindAllStringSubmatch(compilerOutput, -1)
+// // 	if matches == nil {
+// // 		return nil, nil
+// // 	}
+
+// // 	forbiddenFunctionsSet := make(map[string]bool)
+
+// // 	for _, match := range matches {
+// // 		if len(match) > 2 {
+// // 			forbiddenFunctionsSet[match[2]] = true
+// // 		}
+// // 	}
+
+// // 	return setToSlice(forbiddenFunctionsSet), nil
+// // }
+
+// // func handleCompileError(output string) error {
+// // 	usedForbiddenFunctions, parseErr := parseForbiddenFunctions(output)
+// // 	if parseErr != nil {
+// // 		return parseErr
+// // 	} else if len(usedForbiddenFunctions) > 0 {
+// // 		forbiddenFunctions := strings.Join(usedForbiddenFunctions, ", ")
+// // 		return errors.NewSubmissionError(errors.ErrForbiddenItem, forbiddenFunctions)
+// // 	} else {
+// // 		return errors.NewSubmissionError(errors.ErrInvalidCompilation, output)
+// // 	}
+// // }
+
+// // TODO
+// // Currently only handles linting for keywords - code for conditional compilation stands,
+// // just needs to be adapted a little bit.
+// // func Execute(test Exercise.Exercise) (err error) {
+// // 	exercisePath := fmt.Sprintf("compile-environment/%s/%s", test.TurnInDirectory, test.TurnInFiles[0])
+// // 	err = lintStudentCode(exercisePath, test)
+// // 	if err != nil {
+// // 		return err
+// // 	}
+
+// // 	logger.Info.Printf("no forbidden items/keywords found in %s", test.TurnInDirectory+"/"+test.TurnInFiles[0])
+// // 	return nil
+// // }
