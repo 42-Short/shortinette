3e794bde9c49cb8ef58347bc489099999308ebc2
winstonallo <arthurbiedchar@gmail.com>
2024-08-08 13:20:21 +0200

    fix: shortinette package name

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
-package main
+package shortinette
 
 import (
 	"os"
 
-	"github.com/42-Short/shortinette/internal/tests/R00"
-	Module "github.com/42-Short/shortinette/pkg/interfaces/module"
 	"github.com/42-Short/shortinette/pkg/logger"
-	Short "github.com/42-Short/shortinette/pkg/short"
-	"github.com/42-Short/shortinette/pkg/short/testmodes/webhook"
 
 	"github.com/42-Short/shortinette/pkg/requirements"
 )
@@ -24,17 +20,17 @@ func Init() {
 	}
 }
 
-func main() {
-	Init()
-	modules := map[string]Module.Module{
-		"00": *R00.R00(),
-		// TODO: "01": *R01.R01(), // TODO
-		// TODO: "02": *R02.R02(), // TODO
-		// TODO: "03": *R03.R03(), // TODO
-		// TODO: "04": *R04.R04(), // TODO
-		// TODO: "05": *R05.R05(), // TODO
-		// "06": *R06.R06(),
-	}
-	short := Short.NewShort("Rust Piscine 1.0", modules, webhook.NewWebhookTestMode(modules))
-	short.Start("00")
-}
+// func main() {
+// 	Init()
+// 	modules := map[string]Module.Module{
+// 		"00": *R00.R00(),
+// 		// TODO: "01": *R01.R01(), // TODO
+// 		// TODO: "02": *R02.R02(), // TODO
+// 		// TODO: "03": *R03.R03(), // TODO
+// 		// TODO: "04": *R04.R04(), // TODO
+// 		// TODO: "05": *R05.R05(), // TODO
+// 		// "06": *R06.R06(),
+// 	}
+// 	short := Short.NewShort("Rust Piscine 1.0", modules, webhook.NewWebhookTestMode(modules))
+// 	short.Start("00")
+// }
