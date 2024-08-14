f6f40f311dc29eb54ddac6828f1800db6b5a8bbb
winstonallo <arthurbiedchar@gmail.com>
2024-08-14 15:51:02 +0200

    add: docs for logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,3 +1,5 @@
+// Package logger provides logging functionality for various aspects of the application,
+// including standard logging for info and error messages, as well as logging for trace files.
 package logger
 
 import (
@@ -7,20 +9,30 @@ import (
 	"time"
 )
 
+// Loggers for different purposes
 var (
-	Info     *log.Logger
-	Error    *log.Logger
-	File     *log.Logger
-	Exercise *log.Logger
+	Info     *log.Logger // Info logs general informational messages.
+	Error    *log.Logger // Error logs error messages with file and line number information.
+	File     *log.Logger // File logs messages to a specified trace file.
+	Exercise *log.Logger // Exercise logs messages specific to an exercise with a custom prefix.
 )
 
+// GetNewTraceFile generates a new trace file name based on the repository ID and the current timestamp.
+//
+//   - repoID: the unique identifier for the repository
+//
+// Returns a string representing the file path for the new trace file.
 func GetNewTraceFile(repoID string) string {
 	t := time.Now()
 	formattedTime := t.Format("20060102_150405")
 	return fmt.Sprintf("traces/%s-%s.log", repoID, formattedTime)
-
 }
 
+// InitializeTraceLogger sets up the File logger to write to the specified trace file.
+//
+//   - filePath: the path to the trace file to which logs should be written
+//
+// Returns an error if the trace file cannot be created or opened.
 func InitializeTraceLogger(filePath string) (err error) {
 	if err = os.Mkdir("traces", 0655); err != nil && !os.IsExist(err) {
 		return err
@@ -33,6 +45,10 @@ func InitializeTraceLogger(filePath string) (err error) {
 	return nil
 }
 
+// InitializeStandardLoggers initializes the Info, Error, and Exercise loggers
+// with appropriate prefixes and output destinations.
+//
+//   - exerciseId: the identifier for the exercise, used in the Exercise logger's prefix
 func InitializeStandardLoggers(exerciseId string) {
 	Info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
 	Error = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
