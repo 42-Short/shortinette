af15088ee369631e7b61521315c9a3a44c3ed8c3
winstonallo <arthurbiedchar@gmail.com>
2024-10-19 21:03:11 +0200

    add: main skeleteton

--- shortinette/server/server.go
+++ shortinette/server/server.go
@@ -1,1 +1,18 @@
 package server
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func helloWorld(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+}
+
+func Router() (router *gin.Engine) {
+	router = gin.Default()
+
+	router.GET("/admin", helloWorld)
+	return router
+}
