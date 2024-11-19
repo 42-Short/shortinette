971792d96f9ab3f5ede30e3e28dc564dca193f3d
Jakobus42 <Jakob.sadjina@icloud.com>
2024-11-19 01:42:10 +0100

    feat: logger from archive

--- app/logger/logger.go
+++ app/logger/logger.go
@@ -1,26 +1,69 @@
+// logger provides logging functionality for various aspects of the application,
+// including standard logging for info and error messages, as well as logging for trace files.
 package logger
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"time"
 )
 
+// Loggers for different purposes
 var (
-	Info    *log.Logger
-	Warning *log.Logger
-	Error   *log.Logger
+	Info    *log.Logger // Info logs general informational messages.
+	Warning *log.Logger // Warning logs message with warning prefix messages.
+	Error   *log.Logger // Error logs error messages with file and line number information.
+	File    *log.Logger // File logs messages to a specified trace file.
 )
 
-func init() {
-	logFile, err := os.OpenFile("./log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+// GetNewTraceFile generates a new trace file name based on the repository ID and the current timestamp.
+//
+//   - repoID: the unique identifier for the repository
+//
+// Returns a string representing the file path for the new trace file.
+func GetNewTraceFile(repoID string) string {
+	t := time.Now()
+	formattedTime := t.Format("20060102_150405")
+	return fmt.Sprintf("traces/%s-%s.log", repoID, formattedTime)
+}
+
+// InitializeTraceLogger sets up the File logger to write to the specified trace file.
+//
+//   - filePath: the path to the trace file to which logs should be written
+//
+// Returns an error if the trace file cannot be created or opened.
+func InitializeTraceLogger(filePath string) (err error) {
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+	File = log.New(file, "", log.Ldate|log.Ltime)
+	originalWriter := File.Writer()
+	File.SetOutput(&syncWriter{file: file, writer: originalWriter})
+	return nil
+}
 
-	multiWriter := io.MultiWriter(logFile, os.Stdout)
+type syncWriter struct {
+	file   *os.File  // The log file
+	writer io.Writer // The original writer for the logger
+}
 
-	Info = log.New(multiWriter, "[INFO] ", log.Ldate|log.Ltime|log.Lshortfile)
-	Warning = log.New(multiWriter, "[WARNING] ", log.Ldate|log.Ltime|log.Lshortfile)
-	Error = log.New(multiWriter, "[ERROR] ", log.Ldate|log.Ltime|log.Lshortfile)
+// Write writes to the underlying writer and ensures the file is flushed after each write.
+func (w *syncWriter) Write(p []byte) (n int, err error) {
+	n, err = w.writer.Write(p)
+	if err != nil {
+		return n, err
+	}
+	err = w.file.Sync() // Flush to disk after every write
+	return n, err
+}
+
+// init initializes the Info, Warning, and Error loggers
+// with appropriate prefixes and output destinations.
+func init() {
+	Info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
+	Warning = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime)
+	Error = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
