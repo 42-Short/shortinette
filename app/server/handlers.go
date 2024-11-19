a2258a05f1e456a2557c17d7b3a0913a7d42e2ac
Jakobus42 <Jakob.sadjina@icloud.com>
2024-11-20 00:58:50 +0100

    feat: CRUD handlers for API

--- app/server/handlers.go
+++ app/server/handlers.go
@@ -1,1 +1,38 @@
 package server
+
+import (
+	"net/http"
+
+	"github.com/42-Short/shortinette/data"
+	"github.com/gin-gonic/gin"
+)
+
+func InsertItemHandler[T any](dao *data.DAO[T]) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "test"})
+	}
+}
+
+func UpdateItemHandler[T any](dao *data.DAO[T]) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "test"})
+	}
+}
+
+func GetAllItemsHandler[T any](dao *data.DAO[T]) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "test"})
+	}
+}
+
+func GetItemHandler[T any](dao *data.DAO[T]) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "test"})
+	}
+}
+
+func DeleteItemHandler[T any](dao *data.DAO[T]) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "test"})
+	}
+}
--- app/server/router.go
+++ app/server/router.go
@@ -3,6 +3,7 @@ package server
 import (
 	"net/http"
 
+	"github.com/42-Short/shortinette/data"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,7 +11,26 @@ func (s *Server) setupRoutes() {
 	group := s.Handler.Group("/shortinette/v1")
 	group.Use(tokenAuthMiddleware())
 
-	group.GET("/test", func(c *gin.Context) {
+	moduleDAO := data.NewDAO[data.Module](nil)           //TODO
+	participantDAO := data.NewDAO[data.Participant](nil) //TODO
+
+	group.Any("/test", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "test"})
 	})
+
+	group.POST("/modules", InsertItemHandler(moduleDAO))
+	group.POST("/participants", InsertItemHandler(participantDAO))
+
+	group.PUT("/modules/:intra_login/:id", UpdateItemHandler(moduleDAO))
+	group.PUT("/participants/:intra_login", UpdateItemHandler(participantDAO))
+
+	group.GET("/modules", GetAllItemsHandler(moduleDAO))
+	group.GET("/participants", GetAllItemsHandler(participantDAO))
+
+	group.GET("/modules/:intra_login/:id", GetItemHandler(moduleDAO))
+	group.GET("/participants/:intra_login", GetItemHandler(participantDAO))
+
+	group.DELETE("/modules/:intra_login/:id", DeleteItemHandler(moduleDAO))
+	group.DELETE("/participants/:intra_login", DeleteItemHandler(participantDAO))
+
 }
