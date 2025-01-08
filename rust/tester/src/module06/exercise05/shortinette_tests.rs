c3323a2cf55d97bb7fd0bcb8c3182b4928f2252a
winstonallo <arthurbiedchar@gmail.com>
2025-01-08 14:37:03 +0100

    fix: randomize tests for ex05

--- rust/tester/src/module06/exercise05/shortinette_tests.rs
+++ rust/tester/src/module06/exercise05/shortinette_tests.rs
@@ -1,98 +1,145 @@
-
 #[cfg(test)]
 mod tests {
+    use rand::{distributions::Alphanumeric, random, thread_rng, Rng};
+
     use super::*;
+
+    fn rand_string() -> String {
+        thread_rng()
+            .to_owned()
+            .sample_iter(&Alphanumeric)
+            .take(10)
+            .map(char::from)
+            .collect()
+    }
 
     #[test]
     fn test_independent_cloning() {
         let mut tableau = Tableau::new();
-        tableau.push(String::from("Hello"));
-        tableau.push(String::from("Rust"));
+
+        let s0 = rand_string();
+        let s1 = rand_string();
+        let s2 = rand_string();
+
+        tableau.push(s0);
+        tableau.push(s1.clone());
 
         let mut tableau_clone = tableau.clone();
-        tableau_clone.push(String::from("World"));
+        tableau_clone.push(s2.clone());
 
         assert_eq!(tableau.len(), 2);
         assert_eq!(tableau_clone.len(), 3);
 
-        assert_eq!(tableau.pop(), Some(String::from("Rust")));
-        assert_eq!(tableau_clone.pop(), Some(String::from("World")));
+        assert_eq!(tableau.pop().unwrap(), s1);
+        assert_eq!(tableau_clone.pop().unwrap(), s2);
     }
 
     #[test]
     fn test_iteration_with_for_loop() {
         let mut tableau = Tableau::new();
-        tableau.push(10);
-        tableau.push(20);
-        tableau.push(30);
 
-        let mut sum = 0;
-        for value in &*tableau {
-            sum += value;
+        let slice: Vec<u8> = thread_rng()
+            .to_owned()
+            .sample_iter(&Alphanumeric)
+            .take(random::<u16>() as usize)
+            .map(u8::from)
+            .collect();
+
+        for ueight in slice.clone() {
+            tableau.push(ueight);
         }
 
-        assert_eq!(sum, 60);
+        assert_eq!(tableau.len(), slice.len());
+
+        for idx in 0..tableau.len() {
+            assert_eq!(tableau[idx], slice[idx]);
+        }
     }
 
     #[test]
     fn test_deref_mut() {
         let mut tableau = Tableau::new();
-        tableau.push(1);
-        tableau.push(2);
-        tableau.push(3);
+
+        let n0 = random::<i32>();
+        let n1 = random::<i32>();
+        let n2 = random::<i32>();
+        let n3 = random::<i32>();
+        let n4 = random::<i32>();
+
+        tableau.push(n0);
+        tableau.push(n1);
+        tableau.push(n2);
 
         {
             let tableau_mut: &mut [i32] = &mut *tableau;
-            tableau_mut[0] = 10;
-            tableau_mut[2] = 30;
+            tableau_mut[0] = n3;
+            tableau_mut[2] = n4;
         }
 
-        assert_eq!(tableau.pop(), Some(30));
-        assert_eq!(tableau.pop(), Some(2));
-        assert_eq!(tableau.pop(), Some(10));
+        assert_eq!(tableau.pop(), Some(n4));
+        assert_eq!(tableau.pop(), Some(n1));
+        assert_eq!(tableau.pop(), Some(n3));
     }
 
     #[test]
     fn test_with_tuples() {
         let mut tableau = Tableau::new();
-        tableau.push((1, "one"));
-        tableau.push((2, "two"));
-        tableau.push((3, "three"));
+
+        let s0 = rand_string();
+        let s1 = rand_string();
+        let s2 = rand_string();
+
+        let n0 = random::<i32>();
+        let n1 = random::<i32>();
+        let n2 = random::<i32>();
+
+        tableau.push((n0, s0));
+        tableau.push((n1, s1));
+        tableau.push((n2, s2.clone()));
 
         assert_eq!(tableau.len(), 3);
 
         let tuple = tableau.pop();
-        assert_eq!(tuple, Some((3, "three")));
+        assert_eq!(tuple, Some((n2, s2)));
         assert_eq!(tableau.len(), 2);
     }
 
     #[test]
-    fn test_non_copyable_types() {
+    fn test_i32() {
         let mut tableau = Tableau::new();
-        tableau.push(Box::new(10));
-        tableau.push(Box::new(20));
+
+        let n0 = random::<i32>();
+        let n1 = random::<i32>();
+
+        tableau.push(n0);
+        tableau.push(n1);
 
         assert_eq!(tableau.len(), 2);
 
         let value = tableau.pop().unwrap();
-        assert_eq!(*value, 20);
+        assert_eq!(value, n1);
 
         let value = tableau.pop().unwrap();
-        assert_eq!(*value, 10);
+        assert_eq!(value, n0);
     }
 
     #[test]
     fn test_large_push() {
         let mut tableau = Tableau::new();
 
-        for i in 0..1000 {
-            tableau.push(i);
+        let len = random::<u16>();
+        let mut cpy = Vec::new();
+
+        for _ in 0..len {
+            let val = random::<u64>();
+            tableau.push(val);
+            cpy.push(val);
         }
 
-        assert_eq!(tableau.len(), 1000);
+        assert_eq!(tableau.len(), len as usize);
 
-        for i in (0..1000).rev() {
-            assert_eq!(tableau.pop(), Some(i));
+        for _ in (0..len).rev() {
+            assert_eq!(tableau.pop().unwrap(), cpy.pop().unwrap());
         }
 
         assert!(tableau.is_empty());
@@ -113,54 +160,10 @@ mod tests {
     }
 
     #[test]
-    fn test_push_and_pop_multiple() {
-        let mut tableau = Tableau::new();
-
-        tableau.push(1);
-        tableau.push(2);
-        tableau.push(3);
-        tableau.push(4);
-        tableau.push(5);
-
-        assert_eq!(tableau.len(), 5);
-
-        assert_eq!(tableau.pop(), Some(5));
-        assert_eq!(tableau.pop(), Some(4));
-        assert_eq!(tableau.pop(), Some(3));
-        assert_eq!(tableau.len(), 2);
-
-        tableau.push(6);
-        assert_eq!(tableau.len(), 3);
-        assert_eq!(tableau.pop(), Some(6));
-        assert_eq!(tableau.len(), 2);
-    }
-
-    #[test]
     fn test_new_tableau() {
         let tableau: Tableau<i32> = Tableau::new();
         assert_eq!(tableau.len(), 0);
         assert!(tableau.is_empty());
-    }
-
-    #[test]
-    fn test_pop() {
-        let mut tableau = Tableau::new();
-        assert_eq!(tableau.pop(), None);
-
-        tableau.push(5);
-        tableau.push(15);
-        assert_eq!(tableau.len(), 2);
-
-        let popped = tableau.pop();
-        assert_eq!(popped, Some(15));
-        assert_eq!(tableau.len(), 1);
-
-        let popped = tableau.pop();
-        assert_eq!(popped, Some(5));
-        assert_eq!(tableau.len(), 0);
-
-        assert!(tableau.is_empty());
-        assert_eq!(tableau.pop(), None);
     }
 
     #[test]
@@ -180,29 +183,20 @@ mod tests {
     #[test]
     fn test_into_iterator() {
         let mut tableau = Tableau::new();
-        tableau.push("a");
-        tableau.push("b");
-        tableau.push("c");
+
+        let s0 = rand_string();
+        let s1 = rand_string();
+        let s2 = rand_string();
+
+        tableau.push(s0.clone());
+        tableau.push(s1.clone());
+        tableau.push(s2.clone());
 
         let mut iter = tableau.into_iter();
-        assert_eq!(iter.next(), Some("a"));
-        assert_eq!(iter.next(), Some("b"));
-        assert_eq!(iter.next(), Some("c"));
+        assert_eq!(iter.next(), Some(s0));
+        assert_eq!(iter.next(), Some(s1));
+        assert_eq!(iter.next(), Some(s2));
         assert_eq!(iter.next(), None);
-    }
-
-    #[test]
-    fn test_clone() {
-        let mut tableau = Tableau::new();
-        tableau.push(String::from("Hello"));
-        tableau.push(String::from("World"));
-
-        let tableau_clone = tableau.clone();
-        assert_eq!(tableau.len(), tableau_clone.len());
-
-        tableau.push(String::from("!"));
-        assert_eq!(tableau.len(), 3);
-        assert_eq!(tableau_clone.len(), 2);
     }
 
     #[test]
