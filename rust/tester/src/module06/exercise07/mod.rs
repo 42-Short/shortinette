5336990b7f7499d5ec947dcd72928c8916757c13
winstonallo <arthurbiedchar@gmail.com>
2025-01-19 15:50:17 +0100

    add: custom compilation routine for 06-07

--- rust/tester/src/module06/exercise07/mod.rs
+++ rust/tester/src/module06/exercise07/mod.rs
@@ -1,6 +1,9 @@
-use std::path;
+use std::{
+    path::{self, PathBuf},
+    process::Command,
+};
 
-use crate::{repository_path, testable::Testable};
+use crate::{repository_path, result::TestResult, testable::Testable};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Exercise07;
@@ -8,5 +11,40 @@ pub struct Exercise07;
 impl Testable for Exercise07 {
     fn path(&self) -> path::PathBuf {
         repository_path().join("ex07")
+    }
+
+    // "-C", "panic=abort", "-C", "link-args=-nostartfiles", "-o", "ft_putchar", "ft_putchar.rs"
+    fn compile(&self) -> Result<Option<path::PathBuf>, crate::result::TestResult> {
+        self.ensure_path();
+
+        let _ = Command::new("rustc")
+            .arg("-C")
+            .arg("panic=abort")
+            .arg("link-args=-nostartfiles")
+            .arg("-o")
+            .arg("ft_putchar")
+            .arg("ft_putchar.rs")
+            .output()
+            .expect("Failed to compile ft_putchar.rs.");
+
+        Ok(Some(PathBuf::from("ft_putchar.rs")))
+    }
+
+    fn run_test(&self) -> crate::result::TestResult {
+        let executable_path = self.compile().expect("Compilation failed.").unwrap();
+
+        let output = Command::new(executable_path)
+            .output()
+            .expect("Failed to execute ft_putchar.");
+
+        if output.status.code() != Some(42) {
+            return TestResult::Failed;
+        }
+
+        if output.stdout != b"42" {
+            return TestResult::Failed;
+        }
+
+        TestResult::Passed
     }
 }
